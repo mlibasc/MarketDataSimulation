080a32c3ff82f0feee22caf9e0a0bab5b8245adb
mei.li <mei.li@ott.ca>
2021-03-26 13:42:47 -0400

    adapted mongoScript.py to work with ReactiveMongo repo

--- mongoScript.py
+++ mongoScript.py
@@ -1,6 +1,3 @@
-# Created By: Mei Li
-# Date Created: 9-Mar-2021
-# Last Updated: 23-Mar-2021
 '''
 The purpose of this program is to simulate market data for a MongoDB database. 
 
@@ -8,13 +5,12 @@ The purpose of this program is to simulate market data for a MongoDB database.
 
 import pymongo
 import datetime
-# pprint library is used to make output look pretty
-from pprint import pprint
-
 import random
 import string 
 import time
 from random import randrange
+# pprint library is used to make output look pretty
+#from pprint import pprint
 
 # Create a MongoClient to the running mongo instance
 from pymongo import MongoClient
@@ -33,75 +29,18 @@ db = client.sim_db
 # Create a capped collection
 collection_parameters = {"capped" : True, "size" : 2147483648}
 collection = db.create_collection("stocks", **collection_parameters)
-posts = [
-    {"symbol": "AAPL",
-    "price": "2",
-    "bid": "1",
-    "ask": "1",
-    "accvol": "1",
-    "open": "1",
-    "prev": "1",
-    "change": "1",
-    "changepct": "1",
-    "type": "1",
-    "msg":"1"},
-    {"symbol": "GME",
-    "price": "2",
-    "bid": "2",
-    "ask": "2",
-    "accvol": "2",
-    "open": "2",
-    "prev": "2",
-    "change": "2",
-    "changepct": "2",
-    "type": "2",
-    "msg":"2"},
-    {"symbol": "BB",
-    "price": "3",
-    "bid": "3",
-    "ask": "3",
-    "accvol": "3",
-    "open": "3",
-    "prev": "3",
-    "change": "3",
-    "changepct": "3",
-    "type": "3",
-    "msg":"3"},
-    ]
-# Single post 
-'''
-post = {"symbol": "AAPL",
-    "price": "2",
-    "bid": "1",
-    "ask": "1",
-    "accvol": "1",
-    "open": "1",
-    "prev": "1",
-    "change": "1",
-    "changepct": "1",
-    "type": "1",
-    "msg":"1"}
-'''
-
-# Insert into collection
-collection.insert_many(posts)
 
 # Prints collection status
 # pprint(collection)
 
-# Create a list to store stocks
-total = 10
+# Create a list to store stock symbols
 stocks = list()
-
-for i in range(total):
-    symbol = ''.join(random.choices(string.ascii_uppercase, k=3))
-    stocks.append(symbol)
-    #print(symbol, " ", stocks[i])
+stocks = ['AAPL','TSLA','BB','GME','NIO','TME','FEDU','VIPS','EDU','AMC','SPY','BAC','PLTR','XLF','T','GE','F','EEM','NOK','TAL']
 
 # To stop, exit in the cmd prompt
 while(True):
-    # Randomly pick an index for the symbol
-    randInd = randrange(total)
+    # Randomly pick an index for the symbol and output in cmd prompt
+    randInd = randrange(len(stocks))
     symbol = stocks[randInd]
     print(symbol)
 
@@ -109,30 +48,16 @@ while(True):
     price = str(randrange(100))
     bid = str(randrange(100))
     ask = str(randrange(100))
-    accvol = str(randrange(100))
-    openStr = str(randrange(100))
-    prev = str(randrange(100))
-    change = str(randrange(100))
-    changepct = str(randrange(100))
-    typeStr = str(randrange(100))
-    msg = str(randrange(100))
 
     post = {
         "symbol": symbol,
         "price": price,
         "bid": bid,
-        "ask": ask,
-        "accvol": accvol,
-        "open": openStr,
-        "prev": prev,
-        "change": change,
-        "changepct": changepct,
-        "type": typeStr,
-        "msg":msg
+        "ask": ask
     }
     
     # Insert new post into collection
     collection.insert_one(post)
 
-    sleepDuration = randrange(5)
+    sleepDuration = randrange(2)
     time.sleep(sleepDuration)
